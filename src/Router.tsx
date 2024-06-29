d5dea9306fbc187ca77e5a16037d4218f75a4838
gabriel <gabrieldomingos2502@gmail.com>
2024-06-29 00:15:46 -0300

    configurações rotas

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,24 +1,21 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import AppLayout from './componentes/AppLayout';
 import Home from './paginas/Home';
 import FaleConosco from './paginas/FaleConosco';
 import QuemSomos from './paginas/QuemSomos';
 import ErrorPage from './paginas/ErrorPage';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <AppLayout />,
-    errorElement: <ErrorPage />,
-    children: [
-      { path: '/', element: <Home /> },
-      { path: 'quem-somos', element: <QuemSomos /> },
-      { path: 'fale-conosco', element: <FaleConosco /> },
-      // Adicione mais rotas aqui
-    ],
-  },
-]);
-
 export const AppRouter = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Home />} />
+          <Route path="quem-somos" element={<QuemSomos />} />
+          <Route path="fale-conosco" element={<FaleConosco />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Route>
+      </Routes>
+    </HashRouter>
+  );
 };
